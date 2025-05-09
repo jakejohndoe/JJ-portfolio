dc49a07b3f4e70a860801c3f302a82fdcdd9eb38
Jakob Johnson <jakobmjohnson9@gmail.com>
2025-05-09 12:03:37 -0500

    added admin loginn routes to app.tsx, protected route, some redirects, and edited vercel config.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "wouter";
 import Home from "@/pages/Home";
 import BlogList from "@/pages/BlogList";
 import BlogPost from "@/pages/BlogPost";
+import AdminLogin from "@/pages/AdminLogin"; // Add this import
 import AdminDashboard from "@/pages/AdminDashboard";
 import BlogCreation from "@/pages/BlogCreation";
 import UserManagement from "@/pages/UserManagement";
@@ -11,13 +12,14 @@ import SpotlightCursor from "@/components/CustomCursor";
 function App() {
   return (
     <>
-      <SpotlightCursor /> {/* 👈 Tracker rendered above the router */}
+      <SpotlightCursor />
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/blogs" component={BlogList} />
         <Route path="/blogs/:id" component={BlogPost} />
+        <Route path="/admin/login" component={AdminLogin} /> {/* Add this route */}
         <Route path="/admin" component={AdminDashboard} />
-        <Route path="/admin/blogs" component={BlogList} /> {/* Admin blogs listing */}
+        <Route path="/admin/blogs" component={BlogList} />
         <Route path="/admin/blogs/create" component={BlogCreation} />
         <Route path="/admin/users" component={UserManagement} />
         <Route component={NotFound} />
